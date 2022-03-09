9eb24cd218296674044ff4cbf095146866006e8e
makos81 <maciejrajcic@gmail.com>
2022-03-09 21:09:49 +0100

    Adding UUID

--- src/main/java/com/kodilla/transfersgateway/service/TransferProducer.java
+++ src/main/java/com/kodilla/transfersgateway/service/TransferProducer.java
@@ -4,8 +4,11 @@ import com.kodilla.commons.Transfer;
 import com.kodilla.commons.TransferMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.protocol.types.Field;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Slf4j
 @Service
@@ -16,7 +19,7 @@ public class TransferProducer {
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
     public void sendTransfer(final Transfer transfer) {
-        TransferMessage transferMessage = new TransferMessage(transfer);
+        TransferMessage transferMessage = new TransferMessage(UUID.randomUUID(), transfer);
         log.info("Sending message to Kafka, transferMessage: {}", transferMessage);
         kafkaTemplate.send(TRANSFERS_TOPIC, transferMessage);
         log.info("Message was sent");
